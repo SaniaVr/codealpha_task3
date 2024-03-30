5a46260f0c8f09f70eed7ffb2dd96248f55b38de
SaniaVr <145044785+SaniaVr@users.noreply.github.com>
2024-03-30 22:03:47 +0530

    darkmodetoggle.js

--- darkmodetoggle.js
+++ darkmodetoggle.js
@@ -8,12 +8,12 @@ const theme = document.querySelector("#theme-link");
 // Listen for a click on the button
 btn.addEventListener("click", function() {
   // If the current URL contains "ligh-theme.css"
-  if (theme.getAttribute("href") == "resources/css/style.css") {
+  if (theme.getAttribute("href") == "style.css") {
     // ... then switch it to "dark-theme.css"
-    theme.href = "resources/css/dark-theme.css";
+    theme.href = "dark-theme.css";
   // Otherwise...
   } else {
     // ... switch it to "light-theme.css"
-    theme.href = "resources/css/style.css";
+    theme.href = "style.css";
   }
 });
